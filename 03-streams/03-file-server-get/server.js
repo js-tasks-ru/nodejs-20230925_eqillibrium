ca18512434f1bbeb8ebd956d08243a3052c842e2
Sergei Kaliuzhnyi <skaluzhni@sutochno.ru>
2023-10-18 18:33:08 +0300

    hw01-03

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,17 +1,33 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
+
+const isNested = (targetPath) => targetPath.includes('/');
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (isNested(pathname)) {
+    res.statusCode = 400;
+    res.end('Nested routes are not supported');
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
+
+      const fileReadStream = fs.createReadStream(filepath);
+      fileReadStream.pipe(res);
+
+      fileReadStream.on('error', () => {
+        res.statusCode = 404;
+        res.end('Not found');
+      });
 
       break;
 
--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,20 +1,41 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
+const isNested = (pathname) => pathname.includes('/');
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
+
+  if (isNested(pathname)) {
+    res.statusCode = 400;
+    res.end('Nested paths are not supported');
+    return;
+  }
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
+      if (fs.existsSync(filepath)) {
+        fs.unlink(filepath, (err) => {
+          if (err) {
+            res.statusCode = 500;
+            res.end(err.message);
+          } else {
+            res.statusCode = 200;
+            res.end('Deleted');
+          }
+        });
+      } else {
+        res.statusCode = 404;
+        res.end('Not found');
+      }
 
       break;
-
     default:
       res.statusCode = 501;
       res.end('Not implemented');
